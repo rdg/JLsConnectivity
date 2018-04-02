40853c513ecbe70220f2a505044c032e389e2e22
Georg Duemlein <georg@fluxfederation.com>
2018-04-02 13:04:21 +1200

    Initial Commit

---
+++ JLsConnectivity/JLsConnectivityView.h
@@ -0,0 +1,13 @@
+//
+//  JLsConnectivityView.h
+//  JLsConnectivity
+//
+//  Created by Georg Duemlein on 2/04/18.
+//  Copyright © 2018 Georg Duemlein. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface JLsConnectivityView : ScreenSaverView
+
+@end
